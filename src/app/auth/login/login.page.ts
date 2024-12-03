b595732b8f032eb3acd63d1312ac3fe0065e1cd9
Morgan Ballat <mgp.ballat@gmail.com>
2024-12-03 11:43:23 +0100

    some architecture including tasks by projet + login form

---
+++ src/app/auth/login/login.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit
+{
+  public loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit()
+  {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login()
+  {
+    if (this.loginForm.valid)
+    {
+      console.log('Form Submitted', this.loginForm.value);
+      // Ajoutez ici votre logique de connexion
+
+    } else
+    {
+      console.log('Form not valid');
+    }
+  }
+
+  ngSubmit()
+  {
+    console.log('Form Submitted', this.loginForm.value);
+  }
+
+}
